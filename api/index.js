1f005fa656d14e7490943f7935c03b0e39e30443
Hassan <hssnkhatri2@gmail.com>
2024-04-08 20:00:30 +0500

    Deployment update 3

--- api/index.js
+++ api/index.js
@@ -6,23 +6,23 @@ import authRoutes from '../api/route/auth.route.js'
 import cookieParser from 'cookie-parser';
 import postRoutes from '../api/route/post.route.js'
 import commentRoutes from '../api/route/comment.route.js'
-import cors from'cors';
+import cors from 'cors';
 import path from 'path';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => { 
+    .then(() => {
         console.log('Database Connected');
     })
     .catch((err) => {
         console.log(err)
     });
 
-    
 
 const app = express();
 
+const __dirname = path.resolve();
 app.use(cors(
     {
         origin: ["https://insights-blog-8bn9.vercel.app/"],
@@ -39,8 +39,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'build','index.html'))
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
 app.use((err, req, res, next) => {
